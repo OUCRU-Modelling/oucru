5e05be3ed879de8e6c9de5f27a38c081479c6219
Tuyen Huynh <hntuyen282@gmail.com>
2026-04-22 16:48:27 +0700

    move palette gen fns to `cols_and_pal.R`

--- R/cols_and_pal.R
+++ R/cols_and_pal.R
@@ -70,3 +70,30 @@ oucru_list_pals <- function() {
   print(OUCRU_PALS)
   cli_inform(c("i" = "Check the official brand guideline for more information"))
 }
+
+#' Palette generator for discrete scale (used by `ggplot2`)
+#' @param palette OUCRU palette name
+#' @param direction Palette direction, 0 or positive number means forward, negative means backward
+palette_gen_d <- function(palette = "main", direction = 1) {
+  function(n) {
+    if (n > length(oucru_palette(palette))) {
+      stop("Not enough colors in this palette!")
+    } else {
+      all_colors <- oucru_palette(palette)
+      all_colors <- unname(unlist(all_colors))
+      all_colors <- if (direction >= 0) all_colors else rev(all_colors)
+      color_list <- all_colors[1:n]
+    }
+  }
+}
+
+#' Palette generator for continuous scale (used by `ggplot2`)
+#' @param palette OUCRU palette name
+#' @param direction Palette direction, 0 or positive number means forward, negative means backward
+#' @param ... Passed to `grDevices::colorRampPalette()`
+palette_gen_c <- function(palette = "main", direction = 1, ...) {
+  pal <- oucru_palette(palette)
+  pal <- if (direction >= 0) pal else rev(pal)
+
+  colorRampPalette(pal, ...)
+}
